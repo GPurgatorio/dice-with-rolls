acaf3e7df31cf3fd29a588204ce6af11351d8487
Giulio Purgatorio <g.purgatorio@studenti.unipi.it>
2019-10-30 12:29:17 +0100

    Global URLs

---
+++ monolith/urls.py
@@ -0,0 +1,11 @@
+HOME_URL = "http://127.0.0.1:5000/"
+
+LOGIN_URL = HOME_URL + "users/login"
+LOGOUT_URL = HOME_URL + "users/logout"
+
+READ_URL = HOME_URL + "stories"
+SETTINGS_URL = HOME_URL + "stories/new/settings"
+WRITE_URL = HOME_URL + "stories/write"
+SUBMIT_URL = HOME_URL + "stories/new/submit"
+ROLL_URL = HOME_URL + "stories/new/roll"
+REACTION_URL = HOME_URL + "stories/reaction"
