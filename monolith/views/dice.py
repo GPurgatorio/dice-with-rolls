aa29996b31d3a32a2b192782b92890741932af99
damicoglc <damicoglc@gmail.com>
2019-11-04 11:22:38 +0100

    Added selection of dice set

--- monolith/views/dice.py
+++ monolith/views/dice.py
@@ -26,12 +26,24 @@ def _roll_dice():
         dice_number = int(request.form['dice_number'])
         session['dice_number'] = dice_number
         if dice_number not in range(2, 7):
-            raise ValueError
+            raise ValueError('number')
+
+        # get dice set
+        dice_set = request.form['dice_set']
+        session['dice_set'] = dice_set
+        print(dice_set)
+        if dice_set not in {'standard', 'animal', 'halloween'}:
+            raise ValueError('set')
+
     except BadRequestKeyError:  # i'm here after re-rolling dice
         dice_number = session['dice_number']
     except (KeyError, ValueError):  # i'm here directly, have to go from settings before
-        flash('Invalid number of dice!', 'error')
-        session.pop('dice_number', None)
+        if ValueError is 'number':
+            flash('Invalid number of dice!', 'error')
+            session.pop('dice_number', None)
+        else:
+            flash('Invalid dice set!', 'error')
+            session.pop('dice_set', None)
         return redirect(url_for('dice._settings'))
 
     # random sampling dice and throw them
@@ -41,7 +53,7 @@ def _roll_dice():
         try:
             filename = os.path.dirname(os.path.abspath(__file__))
             print(filename)
-            dice_list.append(Die('monolith/resources/die' + str(i) + '.txt'))
+            dice_list.append(Die('monolith/resources/' + dice_set + '/die' + str(i) + '.txt'))
         except FileNotFoundError:
             print("File die" + str(i) + ".txt not found")
             session.pop('dice_number', None)
@@ -56,7 +68,7 @@ def _roll_dice():
         return redirect(url_for('stories._stories', message='Error in throwing dice'))
     session['figures'] = dice_set.pips
 
-    context_vars = {'dice_number': dice_number, 'words': dice_set.pips,
-                    'write_url': WRITE_URL, 'settings_url': SETTINGS_URL}
+    context_vars = {'dice_number': dice_number, 'dice_set': dice_set,
+                    'words': dice_set.pips, 'write_url': WRITE_URL, 'settings_url': SETTINGS_URL}
     return render_template('roll_dice.html', **context_vars)
 
